3d1beb718fb915e6b8837e222ae9c37b51020894
MCooperBorkenhagen <matthew.borkenhagen@gmail.com>
2023-06-28 09:30:47 -0500

    global encoding data structures add

--- scripts/phon_predictions.R
+++ scripts/phon_predictions.R
@@ -1,22 +1,4 @@
-# calculate a range of metrics from predictions and their probabilities
 
-preds$phon_prediction_probabilities_mean = NA
-preds$phon_prediction_distance_mean = NA
-#preds$phon_prediction_probabilities_total = NA
-#preds$phon_prediction_units_total = NA
-
-
-for (i in seq(nrow(preds))){
-
-  #preds$phon_prediction_probabilities_total[i] = length(vector_from_string(preds$phon_prediction_features[i]))
-  #preds$phon_prediction_units_total[i] = length(vector_from_string(preds$phon_prediction_probabilities[i]))
-  preds$phon_prediction_probabilities_mean[i] = mean(vector_from_string(preds$phon_prediction_probabilities[i]))
-  
-  phon_prediction_probability_corrected = correct_unitwise_probabilities(vector_from_string(preds$phon_prediction_probabilities[i]), vector_from_string(preds$phon_target_features[i], flatten = T), strip_final = T)
-  preds$phon_prediction_probabilities_mean[i] = mean(phon_prediction_probability_corrected)
-  preds$phon_prediction_distance_mean[i] = L2(vector_from_string(preds$phon_prediction_features[i]), vector_from_string(preds$phon_target_features[i]))
-  
-}
 
 
 
